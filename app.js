4b5c9b230697355bb891811209fff7820a4a2a05
bailey.compton <bailey.compton@afit.edu>
2022-07-13 16:53:26 -0500

    ok now we cookin with oil - ?print= to print

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // create an express app
 const express = require("express")
 const app = express()
-const url = require("url")
+// const url = require("url")
 
 // use the express-static middleware
 app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-  var q = url.parse(req,true)
-  res.send(q.pathname)
+  // res.send("<h1>Hello World!</h1>")
+  // var q = url.parse(req,true)
+  const toprint = req.query.print;
+  res.send(toprint)
 })
 
 // start the server listening for requests
